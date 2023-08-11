e84820eec0f52e5b13d0822685552671e3415fa2
Edinardo Bezerra <edinardobezerra@yahoo.com.br>
2023-08-11 14:50:30 -0300

    Subtotal & Total methods

--- src/main/java/com/example/webservice/entities/Order.java
+++ src/main/java/com/example/webservice/entities/Order.java
@@ -103,6 +103,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double gettotal() {
+		Double sum = 0.0;
+		for( OrderItem x : items) {
+			sum = sum + x.getsubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
